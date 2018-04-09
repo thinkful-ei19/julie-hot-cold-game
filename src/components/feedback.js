83946de82bfd70a764e13f1bb3d50daf2d9035e1
Julie K <julieskim160@gmail.com>
2018-04-09 13:06:57 -0700

    feedback.js

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import './feedback.css'
+
+
+
+export default function Feedback(props){
+    return (
+        <div className="feedback">
+            <span>{props.feedbackResponse}</span>
+        </div>
+    )
+}
