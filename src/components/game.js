968d078bcc2f10929b564449936a6a3dd54a3e64
Julie K <julieskim160@gmail.com>
2018-04-09 13:51:44 -0700

    game.js

--- src/components/game.js
+++ src/components/game.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import './game.css';
+
+
+export default function Game(props) {
+  return (
+      <div className="game">
+          <h1>HOT or COLD</h1>
+        <form>
+            <legend>Make Your Guess</legend>
+            <p>
+                <input type="text" className="userGuess" />
+            </p>
+            <p className="guessCount">
+                Guess # <span className="active-number">{props.guessNum}</span>
+            </p>
+        </form>
+      </div>
+  )
+}
